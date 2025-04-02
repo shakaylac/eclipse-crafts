7ebf1537302438424176ab72928612cebf99be3e
shakaylac <146593316+shakaylac@users.noreply.github.com>
2025-04-02 13:08:53 -0500

    Create Product.jsx
    created a useState for the img to initially display when the page loads and will hopefully display when the an img is not being hovered over or clicked. I made a side gallery section for the images. I wrote some pseudocode to get. a better idea of what I want the side gallery to do.

---
+++ eclipse-craft/src/stylesheets/Product.jsx
@@ -0,0 +1,27 @@
+import {useState} from react;
+import '..stylesheets/Product';
+
+const [img, setImg] = useState(img);
+
+function Product() {
+  return(
+   <h2 class="title"></h2>
+   
+   <section class="img-display">
+     <img class="initial-img" src="" />
+   </section>
+
+   <section class="side-gallery">
+    <img src="" />
+    <img src="" />
+    <img src="" />
+    <img src="" />
+   </section>
+  )
+}
+
+export default Product
+
+/* onhover and onclick (img in side-gallery) display in img-display */
+
+/* offhover img in img-display returns to initial-img */

a9f5066992747b8437f1c71d07367b849ecda5a7
shakaylac <146593316+shakaylac@users.noreply.github.com>
2025-04-02 12:36:35 -0500

    Update App.jsx
    Added footer component to the app.jsx file.

--- eclipse-craft/src/App.jsx
+++ eclipse-craft/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
 import Product-Gallery from './components/Product-Gallery'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Header />
     <Product-Gallery />
+    <Footer />
   
   )
 }

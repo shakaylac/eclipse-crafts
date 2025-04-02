ef492a467992c4327c1e82bb4c113d2afa357790
shakaylac <146593316+shakaylac@users.noreply.github.com>
2025-04-02 12:35:05 -0500

    Create Footer.jsx
    added section. In section there is a logo and paragraph tag. the paragraph tag is for the copyright. I imported the Footer.css file.

---
+++ eclipse-craft/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../stylesheets/Footer's
+
+function Footer() {
+  return(
+    <section>
+     <logo></logo>
+     <p class="copyright"></p>
+    </section>
+  )
+}
+
+export default Footer

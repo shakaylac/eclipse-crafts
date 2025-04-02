90daee3ef14b0ec06557373b80db44876dda8393
shakayla <kaycnite@gmail.com>
2025-04-02 07:51:27 -0500

    Added header.

---
+++ eclipse-craft/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import '../stylesheets/Header.css'
+
+function Header() {
+  return(
+    <header>
+    <ul>
+        <li>About</li>
+        <li>Cart</li>
+    </ul>
+   </header>
+
+  )
+}
+
+export default Header

47bc00012fceabb25bc8032a08726d146eecb46a
shakaylac <146593316+shakaylac@users.noreply.github.com>
2025-04-02 12:30:23 -0500

    Update App.jsx
    Added product gallery component to the App.jsx file

--- eclipse-craft/src/App.jsx
+++ eclipse-craft/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import Header from './components/Header'
+import Product-Gallery from './components/Product-Gallery'
 
 function App() {
   return (
     <Header />
-
+    <Product-Gallery />
   
   )
 }

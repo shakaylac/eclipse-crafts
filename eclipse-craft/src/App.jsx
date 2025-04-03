f4b6b88bca7476ef90097de241603ac969fc1f85
shakayla <kaycnite@gmail.com>
2025-04-03 07:46:41 -0500

    Created Hero.jsx and Hero.css. This is the hero image for the page. I linked the css file to the jsx and imported the Hero component to the App.jsx.

--- eclipse-craft/src/App.jsx
+++ eclipse-craft/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
 import ProductGallery from './components/ProductGallery'
 import Footer from './components/Footer'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Hero />
       <ProductGallery />
       <Footer />
     </>
---
+++ eclipse-craft/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import '../stylesheets/Hero.css'
+
+function Hero() {
+    return(
+        <img class="hero" src='null'></img>
+    )
+}
+
+export default Hero

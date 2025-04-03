3308ea92f5d85baadf886657bb2b5e565830c155
shakaylac <146593316+shakaylac@users.noreply.github.com>
2025-04-03 10:20:55 -0500

    Create Checkout.jsx Added a section tag and commented some pseudocode so I know what I have to put there. Added a label and form tag for the customer's details(name, address, payment method). Need to look up how to connect Shopify/shipstation to the app.

---
+++ eclipse-craft/src/components/Checkout.jsx
@@ -0,0 +1,19 @@
+import '../stylesheets/Checkout.css'
+
+function Checkout() {
+   return(
+     <>
+     <section class="checkout-items">
+       /*need to upload cust. selected items from cart, from an API, and display it here*/
+     </section>
+     
+     <label for="customer-detail"></label>
+     <form class="cuatomer-details>
+
+
+     </form>
+    </>
+   )
+}
+
+export default Checkout

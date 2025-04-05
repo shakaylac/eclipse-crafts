f6bbc4cdead06976b7c849288d88da0141517859
shakayla <kaycnite@gmail.com>
2025-04-05 13:27:10 -0500

    I added a position of slicky to the header and it moves now. Doing this created the effect I was going for. I've put the hero image back to absolute. I need to see if I can have the header touch the sides like it did before when the position was absolute.

--- eclipse-craft/src/components/ProductGallery.jsx
+++ eclipse-craft/src/components/ProductGallery.jsx
@@ -3,81 +3,85 @@ import '../stylesheets/ProductGallery.css'
 function ProductGallery() {
   return(
   <>
-  <h1>Crochet</h1>
-   <section>
-   <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+   <body>
+      <div class="page">
+         <h1>Crochet</h1>
+            <section>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-    <div class="product">
-       <img src="null"/>
-       <h3 className="product-title">Title</h3>
-       <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
-       <p class="product-price">$0.00</p>
-       <button class="save"></button>
-       <button class="add">Add to Cart</button>
-    </div>
+            <div class="product">
+               <img src="null"/>
+               <h3 className="product-title">Title</h3>
+               <p className="product-description">Lorem ipsum dolor sit, amet consec!</p>
+               <p class="product-price">$0.00</p>
+               <button class="save"></button>
+               <button class="add">Add to Cart</button>
+            </div>
 
-   </section>
+            </section>
+         </div>
+      </body>
    </>
  )
 }

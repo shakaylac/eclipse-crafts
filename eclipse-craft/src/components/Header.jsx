88424e6e34708d7f962bb6842b653a0dce4e57cf
shakayla <kaycnite@gmail.com>
2025-04-06 13:39:34 -0500

    Created an image folder and uploaded a logo image. imported logo image to the header component.

--- eclipse-craft/src/components/Header.jsx
+++ eclipse-craft/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import '../stylesheets/Header.css'
+import logo from '../images/Logo_optima_font.png'
 
 function Header() {
   return(
     <div class="container">
       <header>
+        <img class='logo' src={logo} alt='logo'/>
         <ul>
             <li>About</li>
             <li>Cart</li>

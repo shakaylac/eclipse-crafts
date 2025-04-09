623f4667e26d49524238f143c2a0fbf8260e299b
shakayla <kaycnite@gmail.com>
2025-04-09 11:26:28 -0500

    I deleted the logo from the header as it isn;t needed when the logo is in the header.

--- eclipse-craft/src/components/Header.jsx
+++ eclipse-craft/src/components/Header.jsx
@@ -1,11 +1,9 @@
 import '../stylesheets/Header.css'
-import logo from '../images/Logo_optima_font.png'
 
 function Header() {
   return(
     <div class="container">
       <header>
-        <img class='logo' src={logo} alt='logo'/>
         <ul>
             <li>About</li>
             <li>Cart</li>

3ff96cc3449517b4e1ef9df64acc1868cd412d0f
shakayla <kaycnite@gmail.com>
2025-04-09 10:43:12 -0500

    Imported hero image into Hero.jsx file and addedit to the scr attribute.

--- eclipse-craft/src/components/Hero.jsx
+++ eclipse-craft/src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import '../stylesheets/Hero.css'
+import hero from '../images/Hero_1280_300.png'
+
 
 function Hero() {
     return(
-        <img class="hero" src='null'></img>
+        <img class="hero" src={ hero }></img>
     )
 }
 

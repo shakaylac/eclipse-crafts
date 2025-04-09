c8c9e98cbdfd7b97a0ef816d10c777a536784ecd
shakayla <kaycnite@gmail.com>
2025-04-09 11:25:09 -0500

    Added the logo image to the hero image. I'll look better over the hero image.

--- eclipse-craft/src/components/Hero.jsx
+++ eclipse-craft/src/components/Hero.jsx
@@ -1,10 +1,14 @@
 import '../stylesheets/Hero.css'
 import hero from '../images/Hero_1280_300.png'
+import logo from '../images/Logo_optima_font.png'
 
 
 function Hero() {
     return(
-        <img class="hero" src={ hero }></img>
+        <>
+            <img class="hero" src={ hero } />
+            <img className='logo' src={ logo }/>
+        </>
     )
 }
 
